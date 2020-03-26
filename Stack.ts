ffa1d4b7921baca9fac6b105b2c0ad8f84bcb92a
ybbb <1743981534@qq.com>
2020-03-26 16:25:39 +0800

    顺序栈

--- Stack.ts
+++ Stack.ts
@@ -1,11 +1,46 @@
 {
     class Stack {
-        maxLength: number
-        constructor(maxLength: Number) {
-            if (typeof (maxLength) !== 'number') {
-                throw new Error('Failed to init, max length canbe a number!');
+        data: Array<any>;
+        top: number; // 栈顶
+        size: number;
+        constructor(maxLength: number) {
+            this.data = new Array(maxLength);
+            this.top = -1;
+            this.size = maxLength;
+        }
+        push(value: any) {
+            if (this.top === this.size - 1) {
+                throw new Error('Overflow!');
             }
-            this.maxLength = maxLength;
+            this.top++;
+            this.data[this.top] = value;
+        }
+        pop() {
+            if (this.top === -1) {
+                throw new Error('Empty stack!');
+            }
+            const element = this.data[this.top];
+            this.top--;
+            this.data.length--;
+            return element;
+        }
+        getLength() {
+            return this.top + 1;
+        }
+        print() {
+            for (const iterator of this.data) {
+                console.log(iterator);
+            }
         }
     }
+    const stack = new Stack(5);
+    stack.push(1);
+    stack.push(3);
+    stack.push(2);
+    stack.push(4);
+    stack.push(5);
+    stack.pop();
+    console.log('栈内元素数量：', stack.getLength());
+    stack.print();
+
 }
